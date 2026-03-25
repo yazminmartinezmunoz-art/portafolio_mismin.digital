7322eb80c60c3dc3d7fd90733e34d5dd44bc6ad5
Mismin <yazmin.martinez.munoz@gmail.com>
2026-03-25 16:47:48 -0300

    arreglos de image y video en index

--- js/video-hover.js
+++ js/video-hover.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
-*/
+
 
 
 document.querySelectorAll(".allprojects-card").forEach(card => {
@@ -37,5 +37,30 @@ document.querySelectorAll(".allprojects-card").forEach(card => {
     video.pause();
     video.currentTime = 0;
   });
+});
+
+*/
+
+document.querySelectorAll(".project-card").forEach(card => {
+
+  const media = card.querySelector(".project-hover-media");
+
+  if (!media) return;
+
+  // SOLO si es video
+  if (media.tagName === "VIDEO") {
+
+    card.addEventListener("mouseenter", () => {
+      media.currentTime = 0;
+      media.play();
+    });
+
+    card.addEventListener("mouseleave", () => {
+      media.pause();
+      media.currentTime = 0;
+    });
+
+  }
+
 });
 
